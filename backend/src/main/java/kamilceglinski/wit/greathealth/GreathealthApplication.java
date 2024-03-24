2772e545cb2786df4fb4fe1d61ceb82ecb8d00ab
Kamil Cegliński <kamilp06@gmail.com>
2024-03-24 18:30:57 +0100

    backend init

---
+++ backend/src/main/java/kamilceglinski/wit/greathealth/GreathealthApplication.java
@@ -0,0 +1,13 @@
+package kamilceglinski.wit.greathealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreathealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreathealthApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/kamilceglinski/wit/greathealth/data/entity/PatientEntity.java
@@ -0,0 +1,8 @@
+package kamilceglinski.wit.greathealth.data.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class PatientEntity {
+
+}
