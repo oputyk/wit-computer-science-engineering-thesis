9ada7a82992dbdac7c7052497c7d93e2a2aff262
Kamil Cegliński <kamilp06@gmail.com>
2025-04-05 18:21:23 +0200

    services page

--- frontend/src/app/service-form/service-form.component.ts
+++ frontend/src/app/service-form/service-form.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Patient } from '../api/models/patient';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Service } from '../api/models/service';
+import { Specialty } from '../api/models/specialty';
+import { SpecialtyApiService } from '../api/services/specialty-api.service';
 
 @Component({
   selector: 'app-service-form',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatCardModule,
+    MatSelectModule,
+  ],
   templateUrl: './service-form.component.html',
   styleUrl: './service-form.component.css'
 })
 export class ServiceFormComponent {
+  specialties: Specialty[];  
+  specialtyApiService = inject(SpecialtyApiService)
+  readonly dialogRef = inject(MatDialogRef<ServiceFormComponent>);
+  readonly service = structuredClone(inject<Service>(MAT_DIALOG_DATA));
+
+  ngOnInit(): void {
+    this.specialtyApiService.getAllSpecialties().subscribe(data => {
+      this.specialties = data;
+    })
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    this.dialogRef.close(this.service);
+  }
 
 }
--- frontend/src/app/services/services.component.ts
+++ frontend/src/app/services/services.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ServiceApiService } from '../api/services/service-api.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { Service } from '../api/models/service';
+import { MatDialog } from '@angular/material/dialog';
+import { ServiceRequest } from '../api/models/service-request';
+import { ServiceFormComponent } from '../service-form/service-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [MatTableModule, CommonModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+  serviceApiService = inject(ServiceApiService)
+  displayedColumns: string[] = ['name', 'timeInMinutes', 'specialty', 'action'];
+  dataSource: MatTableDataSource<Service>;
+  readonly dialog = inject(MatDialog);
 
+  ngOnInit(): void {
+    this.refreshTable();
+  }
+
+  create(): void {
+    const dialogRef = this.dialog.open(ServiceFormComponent, {
+      data: {},
+      height: '400px',
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result !== undefined) {
+        const service = result as Service
+        this.serviceApiService.createService({
+          name: service.name,
+          timeInMinutes: service.timeInMinutes,
+          specialtyUuid: service.specialty.uuid
+        } as ServiceRequest).subscribe(res => {
+          this.refreshTable();
+        })
+      }
+    });
+  }
+
+  delete(service: Service): void {
+    this.serviceApiService.deleteService(service.uuid).subscribe(res => {
+      this.refreshTable();
+    })
+  }
+
+  refreshTable(): void {
+    this.serviceApiService.getAllServices().subscribe(data => {
+      this.dataSource = new MatTableDataSource<Service>(data);
+    })
+  }
 }
